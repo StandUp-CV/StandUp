bcf20514385b8c388dd87cadfb600d3a6aabd4f4
gormed <hans.ferchland@gmx.de>
2012-11-29 13:02:00 +0100

    Gui attempts
    Signed-off-by: gormed <hans.ferchland@gmx.de>

---
+++ Standup/Standup_Ogre_Win32/src/StandupGUI.cpp
@@ -0,0 +1,12 @@
+#include "StdAfx.h"
+#include "StandupGUI.h"
+
+
+StandupGUI::StandupGUI(void)
+{
+}
+
+
+StandupGUI::~StandupGUI(void)
+{
+}
---
+++ Standup/Standup_Ogre_Win32/src/StandupGUI.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef __StandupGUI_h_
+#define __StandupGUI_h_
+
+#include "stdafx.h"
+#include <OgreSceneManager.h>
+
+class StandupGUI : public SceneNode
+{
+public:
+	StandupGUI(void);
+	~StandupGUI(void);
+};
+
+#endif 
