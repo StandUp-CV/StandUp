08dd4bef7d0149a18af46905f4934ab2a5a25fbf
daired <monchichi89>
2012-12-12 23:23:25 +0100

    Clean AnimationBuilder.cpp; YRotation Animation
    Signed-off-by: daired

--- Standup/Standup_Ogre_Win32/src/AnimationBuilder.h
+++ Standup/Standup_Ogre_Win32/src/AnimationBuilder.h
@@ -9,7 +9,11 @@ class AnimationBuilder
 public:
 	AnimationBuilder(void);
 	~AnimationBuilder(void);
-	void createAnimations( CEGUI::String v1,  CEGUI::String v2,  CEGUI::String v3);
+	
+	void createAnimations( CEGUI::String v1, CEGUI::String v2, CEGUI::String v3, CEGUI::String r1, CEGUI::String r2, CEGUI::String r3 );
+	void yRotation(CEGUI::Animation* anim,CEGUI::String valueStart, CEGUI::String value);
+	void unifiedXPositionInterpolation(CEGUI::Animation* anim, CEGUI::String pos1, CEGUI::String pos2);
+	CEGUI::Animation* createAnimation(CEGUI::String name, float duration, CEGUI::Animation::ReplayMode replayMode);
 };
 
 #endif // #ifndef __AnimationBuilder_h_
